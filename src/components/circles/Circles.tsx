97ff4399600348a64837368f969468835dd7fe63
Vladyslav Podrezenko <pakgpcoans@gmail.com>
2023-12-03 17:31:59 +0200

    Refactor. Added interactivity. Implemented initial charting.

--- src/components/circles/Circles.tsx
+++ src/components/circles/Circles.tsx
@@ -1,22 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Circles.module.scss';
+import { ThermalSensorSize, useApparateContext } from '../apparate';
+import { useChartContext } from '../chart';
 
+type ThermalSensorValue = {
+    s: number,
+    voltage: number
+}
 
+const thermalSensorValues = new Map<ThermalSensorSize, ThermalSensorValue>([
+    ['LARGE', {s: 491, voltage: 37}],
+    ['MEDIUM', {s: 380, voltage: 41}],
+    ['SMALL', {s: 177, voltage: 58}],
+])
 
 export const Circles = () => {
-    const [selectedCircle, setSelectedCircle] = useState('');
+    const { setThermalSensorSize, setVoltage, thermalSensorSize, currentToggle } = useApparateContext()
+    const { addPoint } = useChartContext('PHOTORESISTOR')
 
-    const handleButtonClick = (size: string) => {
-        setSelectedCircle(size);
+    const handleButtonClick = (size: ThermalSensorSize) => {
+        setThermalSensorSize(size)
+        if(currentToggle == 3) {
+            console.log(currentToggle)
+            const value = thermalSensorValues.get(size)!
+            setVoltage(value.voltage)
+            addPoint(value.s.toString(), { x: value.s, y: value.voltage })
+        }
     };
 
-    const renderSelectedCircle = () => {
-        return (
-            <div className={styles.selectedCircle}>
-                <button className={styles[selectedCircle]}>{selectedCircle}</button>
-            </div>
-        );
-    };
+    useEffect(() => {
+        if(thermalSensorSize){
+            handleButtonClick(thermalSensorSize)
+        }
+    }, [currentToggle])
 
     return (
         <div>
@@ -25,25 +41,23 @@ export const Circles = () => {
             <div className={styles.wrapper}>
                 <button
                     className={styles.circleBig}
-                    onClick={() => handleButtonClick('circleBig')}
+                    onClick={() => handleButtonClick('LARGE', 491, 37)}
                 >
                     491мм<sup>2</sup>
                 </button>
                 <button
                     className={styles.circleMedium}
-                    onClick={() => handleButtonClick('circleMedium')}
+                    onClick={() => handleButtonClick('MEDIUM', 380, 41)}
                 >
                     380мм<sup>2</sup>
                 </button>
                 <button
                     className={styles.circleSmall}
-                    onClick={() => handleButtonClick('circleSmall')}
+                    onClick={() => handleButtonClick('SMALL', 177, 58)}
                 >
                     177мм<sup>2</sup>
                 </button>
             </div>
-
-            {renderSelectedCircle()}
         </div>
     );
 };
--- src/components/voltmeter/Voltmeter.tsx
+++ src/components/voltmeter/Voltmeter.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useRef, useState } from 'react'
+import { useApparateContext } from '../apparate'
 import styles from './Voltmeter.module.scss'
 
 export const Voltmeter = () => {
+    const {voltage} = useApparateContext()
+    const [previousVoltage, setPreviousVoltage] = useState(0)
+    const indicator = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        indicator.current?.animate([
+            { transform: `translateX(${previousVoltage}px)` },
+            { transform: `translateX(${voltage}px)` }
+        ], { duration: 1000 })
+        setTimeout(() => setPreviousVoltage(voltage), 1000)
+    }, [voltage])
+
     return (
         <div>
             <div className={styles.numbers}>
@@ -11,8 +25,8 @@ export const Voltmeter = () => {
                 <div>100</div>
             </div>
 
-            <div className={styles.scale}></div>
-            <div className={styles.vIndicator}></div>
+            <div className={styles.scale}/>
+            <div className={styles.vIndicator} style={{transform: `translateX(${voltage}px)`}} ref={indicator}/>
             <div className={styles.vLabel}>V</div>
         </div>
     )
